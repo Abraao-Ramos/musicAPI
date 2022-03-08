f6e74823c9a126aa45de10745d01051f1c64481e
Abraham Abrahamvan Van Ramos <abrahamvan@Abrahams-MacBook-Pro.local>
2022-03-08 15:52:13 +0000

    first commit :wq wq :wq

---
+++ src/test/java/com/code/musicAPI/MusicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.musicAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
