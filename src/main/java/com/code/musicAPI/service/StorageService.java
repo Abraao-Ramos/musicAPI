ae3b09ab55cef82acf7560a51dc31740985898c4
paddy <paddyrks@gmail.com>
2022-03-20 13:51:11 +0000

    localhost for mysql connection and missing service annotation

--- src/main/java/com/code/musicAPI/service/StorageService.java
+++ src/main/java/com/code/musicAPI/service/StorageService.java
@@ -3,9 +3,11 @@ package com.code.musicAPI.service;
 import com.code.musicAPI.model.Song;
 import com.code.musicAPI.repository.SongRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class StorageService {
 
     @Autowired
